ae847d79a061b428d7c558de3c8e476d29b14fbf
wjdgus06 <63927229+wjdgus06@users.noreply.github.com>
2024-04-29 13:16:07 +0900

    update

--- remote_exec/remote_script_runner.sh
+++ remote_exec/remote_script_runner.sh
@@ -5,14 +5,19 @@ REPO_URL="https://github.com/ehfhfhd/Sonicboom.git"
 SCRIPT_PATH="CentOS6/CentOS6.sh"
 SCRIPT_NAME="CentOS6.sh"
 RESULT_FILE="result.json"
-LOCAL_PATH="/home/jeyoo/tmp/Sonicboom" #취약점 진단 스크립트 위치
+LOCAL_PATH="/home/ubuntu/tmp/Sonicboom" #취약점 진단 스크립트 위치
+# 중앙서버에서는 root계정으로 접속
 
 # 로컬에 레포지토리 클론
 git clone "$REPO_URL" "$LOCAL_PATH"
 
+mkdir -p /home/ubuntu/make_xlsx
+cp /home/ubuntu/tmp/Sonicboom/make_python.py /home/ubuntu/make_xlsx
+cp /home/ubuntu/tmp/Sonicboom/tmp.xlsx /home/ubuntu/make_xlsx
+
 # 원격 서버 목록
 SERVER_LIST=(
-    "ubuntu@3.38.165.230"
+    "centos@3.38.165.230" # 사용자@ip주소
     # "centos@xxx.xxx.xxx.xxx" # 다른 서버 계속...
 )
 
@@ -23,14 +28,13 @@ for SERVER in "${SERVER_LIST[@]}"; do
     HOST=$(echo "$SERVER" | cut -d'@' -f2)
 
     # 스크립트 전송
-    scp "$LOCAL_PATH/$SCRIPT_PATH" "$SERVER:/home/$USER/"
+    scp -o StrictHostKeyChecking=no "$LOCAL_PATH/$SCRIPT_PATH" "$SERVER:/home/$USER/"
 
     # 스크립트 실행
-    ssh "$SERVER" "sudo bash /home/$USER/$SCRIPT_NAME"
+    ssh -o StrictHostKeyChecking=no  "$SERVER" "sudo bash /home/$USER/$SCRIPT_NAME"
 
     # 결과 파일 전송
     # 결과 파일 가져오기
-    scp "$SERVER:/home/$USER/$RESULT_FILE" "/srv/${SERVER}.json"
+    scp -o StrictHostKeyChecking=no "$SERVER:/home/$USER/$RESULT_FILE" "/srv/${SERVER}.json"
 
 done
-
