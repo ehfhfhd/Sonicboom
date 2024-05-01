5ea6ead88096dc4d4eb7d52651a862b3c7357b16
wjdgus06 <63927229+wjdgus06@users.noreply.github.com>
2024-05-01 19:06:40 +0900

    update

--- remote_exec/remote_script_runner.sh
+++ remote_exec/remote_script_runner.sh
@@ -11,15 +11,17 @@ LOCAL_PATH="/home/ubuntu/tmp/Sonicboom" #취약점 진단 스크립트 위치
 # 로컬에 레포지토리 클론
 git clone "$REPO_URL" "$LOCAL_PATH"
 
+mkdir -p "/srv/SNB_서버진단"
+
 mkdir -p /home/ubuntu/make_xlsx
-cp /home/ubuntu/tmp/Sonicboom/make_python.py /home/ubuntu/make_xlsx
+cp /home/ubuntu/tmp/Sonicboom/make_result.py /home/ubuntu/make_xlsx
 cp /home/ubuntu/tmp/Sonicboom/tmp.xlsx /home/ubuntu/make_xlsx
 
-# 원격 서버 목록
-SERVER_LIST=(
-    "centos@3.38.165.230" # 사용자@ip주소
-    # "centos@xxx.xxx.xxx.xxx" # 다른 서버 계속...
-)
+# 원격 서버 목록파일에서 읽기
+SERVER_LIST=()
+while IFS= read -r line; do
+	SERVER_LIST+=("$line")
+done < "/home/ubuntu/servers_list.txt"
 
 # 각 서버에 스크립트 실행 및 결과 파일 전송
 for SERVER in "${SERVER_LIST[@]}"; do
@@ -33,8 +35,15 @@ for SERVER in "${SERVER_LIST[@]}"; do
     # 스크립트 실행
     ssh -o StrictHostKeyChecking=no  "$SERVER" "sudo bash /home/$USER/$SCRIPT_NAME"
 
+    #결과 파일명에 날짜 시간 추가보류
+    NOW=$(date +%Y%m%d_%H%M%S)
+    RESULT_FILENAME="${NOW}_result.json"
+
+
     # 결과 파일 전송
     # 결과 파일 가져오기
-    scp -o StrictHostKeyChecking=no "$SERVER:/home/$USER/$RESULT_FILE" "/srv/${SERVER}.json"
+    scp -o StrictHostKeyChecking=no "$SERVER:/home/$USER/$RESULT_FILE" "/srv/SNB_서버진단/$RESULT_FILENAME"
 
 done
+# 엑셀 파일 추출
+# python3 /home/ubuntu/make_xlsx/make_result.py
